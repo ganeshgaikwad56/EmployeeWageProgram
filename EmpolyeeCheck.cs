c20a68565ef7b8419dde3e10b73654074884bafa
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-21 14:48:36 +0530

    Adding Employee Check Program file Whether Present or Absent

---
+++ EmpolyeeCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class EmpolyeeCheck
+    {
+        public static void EmployeePresentOrNot()
+
+        {
+           // int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 1;
+            //int EMP_RATE_PER_HOUR = 20;
+
+            //int EmpHrs = 0;
+            //int EmpWage = 0;
+
+            Random random = new Random();
+            int Employee = random.Next(2);
+
+            if (Employee == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
